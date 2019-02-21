6ec00fa2b14154e77cf4e14970dfbe33f788d1c1
erobert <etiennerobert33@gmail.com>
2019-02-21 04:46:15 +0100

    improve Parser

--- Parser.py
+++ Parser.py
@@ -1,24 +1,46 @@
 from Implication import Implication
 from NamedProposition import NamedProposition
 from System import System
+from ParseError import ParseError
 
 class Parser:
+  priority = {'=>': 1}
   def __init__(self, text_input):
     self.text_input = text_input
   
   def parse(self):
       self.named_propositions = []
-      self.propositions = \
-        [self.parse_proposition(line) for line in self.text_input.splitlines()]
+      self.propositions = []
+      for line in self.text_input.splitlines():
+          try:
+            self.propositions.append(self.parse_proposition(line))
+          except IndexError:
+            raise ParseError('Incorrect line: "' + line + '"')
       return System(self.named_propositions, self.propositions)
 
+  def create_proposition(self, a, op, b):
+      if op == '=>':
+        return Implication(a, b)
+      raise ParseError('Unknown operand: ' + op)
+
   def parse_proposition(self, line):
-      line = line.replace(' ', '')
-      if line.find('=>') == -1:
-        return self.get_named_proposition(line)
-      split = line.split('=>')
-      return Implication(self.get_named_proposition(split[0]), \
-                         self.get_named_proposition(split[1]))
+      items = line.split(' ')
+      items.reverse()
+
+      a = self.get_named_proposition(items.pop())
+      if not items:
+        return a
+      op = items.pop()
+      return self.parse_items(a, op, items)
+
+  def parse_items(self, a, op, items):
+      b = self.get_named_proposition(items.pop())
+      if not items:
+        return self.create_proposition(a, op, b)
+      next_op = items.pop()
+      if self.priority[op] > self.priority[next_op]:
+        return self.parse_items(self.create_proposition(a, op, b), next_op, items)
+      return self.create_proposition(a, op, self.parse_items(b, next_op, items))
 
   def get_named_proposition(self, name):
       prop = next((p for p in self.named_propositions if p.name == name), None)
