f984bb4cbaa4bf9cac1586a4e56441ac5f794256
erobert <etiennerobert33@gmail.com>
2019-02-21 06:31:45 +0100

    improve interface

--- expertsystem.py
+++ expertsystem.py
@@ -1,10 +1,31 @@
 from System import System
 from Parser import Parser
+from ParseError import ParseError
+import sys
 
-with open('file', 'r') as f:
-  s = Parser(f.read()).parse()
-  s.solve()
-  print('Input:')
+def usage():
+  print('usage: ' + sys.argv[0] + ' input_file')
+  print('Your file should have newline separated logical propositions')
+  print('Every operand and variable should be separated with a space')
+
+if len(sys.argv) != 2:
+  usage()
+  sys.exit()
+with open(sys.argv[1], 'r') as f:
+  try:
+    s = Parser(f.read()).parse()
+  except ParseError as e:
+    print('The given file has a format error: ' + str(e))
+    usage()
+    sys.exit()
+
+  try:
+    s.solve()
+  except IncoherenceError as e:
+    print('The given system has an incoherence : ' + str(e))
+
+  print('System to solve:')
   print(s.propositions)
   print('Result:')
-  print(s.variables)
+  for v in s.variables:
+    print(v.name + ': ' + str(v.state))
